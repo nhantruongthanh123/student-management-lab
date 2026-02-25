fb6bf4f4ac0b826ae0ff456c507b0559882f68e9
Nhân <thanhnhanqs2005@gmail.com>
2026-02-25 14:19:08 +0700

    Add lab2 backend restAPI

---
+++ src/main/java/vn/edu/hcmut/cse/adse/lab/controller/StudentController.java
@@ -0,0 +1,29 @@
+package vn.edu.hcmut.cse.adse.lab.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vn.edu.hcmut.cse.adse.lab.entity.Student;
+import vn.edu.hcmut.cse.adse.lab.service.StudentService;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+    @Autowired
+    private StudentService service;
+    // 1. API Lay danh sach: GET http://localhost:8080/api/students
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return service.getAll();
+    }
+    // 2. API Lay chi tiet: GET http://localhost:8080/api/students/{id}
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Integer id) {
+        return service.getById(id);
+    }
+}
---
+++ src/main/java/vn/edu/hcmut/cse/adse/lab/service/StudentService.java
@@ -0,0 +1,23 @@
+package vn.edu.hcmut.cse.adse.lab.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import vn.edu.hcmut.cse.adse.lab.entity.Student;
+import vn.edu.hcmut.cse.adse.lab.repository.StudentRepository;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository repository;
+
+    public List<Student> getAll() {
+        return repository.findAll();
+    }
+
+    public Student getById(Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+}
